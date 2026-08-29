9c8110b8610d96a16f094df360c970846a3e8dde
Aagaman Pokhrel <aagamanpokharel45@gmail.com>
2026-08-29 22:51:09 +0530

    Trash removal

--- backend/scripts/retrieve.py
+++ backend/scripts/retrieve.py
@@ -1,3 +1,18 @@
 # TODO : Fetch required event from blockchain
 #
 #
+from ape import accounts, project
+
+def main():
+    account = accounts.load("meta_mask")
+
+    contract_address = "" #use from .env
+    contract = project.Store.at(contract_address)
+
+    val = [] # the required values
+    tx_receipt = contract.anchor_certificate(val, sender=account)
+
+    # 3. Read the logged event
+    events = list(tx_receipt.decode_logs(project.Store.CertificateAnchored))
+    if events:
+        print(f"Success! The new issue is: {events[0].target_hash}")
