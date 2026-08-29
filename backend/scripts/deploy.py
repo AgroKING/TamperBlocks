4a69550c147c19742472a049f6ce1a875f5a6299
Aagaman Pokhrel <aagamanpokharel45@gmail.com>
2026-08-29 17:17:40 +0530

    Added Deployment Script

--- backend/scripts/deploy.py
+++ backend/scripts/deploy.py
@@ -1,1 +1,4 @@
 # Using web3 for deploying smartcontracts!
+from ape import project,accounts
+account = accounts.load("<ALIAS>") ## TODO : use .env for safe loading
+contract = project.get_contract("store")
