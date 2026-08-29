75bf1d95842984cbd08b3a96449e34fe5429bcbb
Aagaman Pokhrel <aagamanpokharel45@gmail.com>
2026-08-30 01:01:08 +0530

    Hash querrying implementation

--- backend/scripts/retrieve.py
+++ backend/scripts/retrieve.py
@@ -3,16 +3,21 @@
 #
 from ape import accounts, project
 
-def main():
-    account = accounts.load("meta_mask")
+# Getter for targetHash
+def get_hash(CID):
+    record = contract.get_certificate_by_cid("CID")
+    return record.target_hash if hasattr(record, "target_hash") else record
 
-    contract_address = "" #use from .env
-    contract = project.Store.at(contract_address)
 
-    val = [] # the required values
-    tx_receipt = contract.anchor_certificate(val, sender=account)
+account = accounts.load("meta_mask")
 
-    # 3. Read the logged event
-    events = list(tx_receipt.decode_logs(project.Store.CertificateAnchored))
-    if events:
-        print(f"Success! The new issue is: {events[0].target_hash}")
+contract_address = ""  # use from .env
+contract = project.Store.at(contract_address)
+
+val = []  # the required values
+tx_receipt = contract.anchor_certificate(*val, sender=account)
+
+# 3. Read the logged event
+events = list(tx_receipt.decode_logs(project.Store.CertificateAnchored))
+if events:
+    print(f"Success! The new issue is: {events[0].target_hash}")
