f24fb6d64fe050fa21087bd43337cece298c690e
Aagaman Pokhrel <aagamanpokharel45@gmail.com>
2026-08-30 01:03:27 +0530

    Proper hash loader

--- backend/scripts/retrieve.py
+++ backend/scripts/retrieve.py
@@ -3,6 +3,7 @@
 #
 from ape import accounts, project
 
+
 # Getter for targetHash
 def get_hash(CID):
     record = contract.get_certificate_by_cid("CID")
@@ -13,11 +14,13 @@ account = accounts.load("meta_mask")
 
 contract_address = ""  # use from .env
 contract = project.Store.at(contract_address)
-
 val = []  # the required values
-tx_receipt = contract.anchor_certificate(*val, sender=account)
 
-# 3. Read the logged event
-events = list(tx_receipt.decode_logs(project.Store.CertificateAnchored))
-if events:
-    print(f"Success! The new issue is: {events[0].target_hash}")
+
+def load_hash(val):
+    tx_receipt = contract.anchor_certificate(*val, sender=account)
+
+    # 3. Read the logged event
+    events = list(tx_receipt.decode_logs(project.Store.CertificateAnchored))
+    if events:
+        print(f"Success! The new issue is: {events[0].target_hash}")
