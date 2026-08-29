94d3a32232227434e15e9c69dae7298427fd6025
Aagaman Pokhrel <aagamanpokharel45@gmail.com>
2026-08-30 01:41:39 +0530

    Updated deployment script

--- backend/scripts/deploy.py
+++ backend/scripts/deploy.py
@@ -1,4 +1,8 @@
 # Using web3 for deploying smartcontracts!
 from ape import project,accounts
-account = accounts.load("<ALIAS>") ## TODO : use .env for safe loading
-contract = project.get_contract("store")
+
+def main():
+    account = accounts.load("meta_mask")  # TODO: use .env for safe loading
+    contract = account.deploy(project.Store)
+    print(f"Store deployed at: {contract.address}")
+    return contract
